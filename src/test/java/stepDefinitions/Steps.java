32689e225b14aad105b2aa9e317b5acfdf07e2a9
nHs2415 <nuwanihansika92@gmail.com>
2025-12-23 13:32:41 +0530

    implementing step definitions for login features

---
+++ src/test/java/stepDefinitions/Steps.java
@@ -0,0 +1,63 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import pageObjects.LoginPage;
+
+public class Steps {
+    public WebDriver driver;
+    public LoginPage lp;
+
+    @Given("User Launch Chrome Browser")
+    public void user_launch_chrome_browser() {
+        System.setProperty("webdriver.chrome.driver",System.getProperty("user")+"//Drivers//chromedriver.exe");
+        driver = new ChromeDriver();
+        lp = new LoginPage(driver);
+    }
+
+    @When("User opes URL {string}")
+    public void user_opes_url(String url) {
+        driver.get(url);
+    }
+
+    @When("User enters Email as {string} and Password as {string}")
+    public void user_enters_email_as_and_password_as(String email, String password){
+        lp.setUserName(email);
+        lp.setPassword(password);
+    }
+
+    @When("Click on Login")
+    public void click_on_login() {
+        lp.clickLogin();
+    }
+
+    @Then("Page Title should be {string}")
+    public void page_title_should_be(String title) {
+        if(driver.getPageSource().contains("Login was unsuccessful.")){
+            driver.close();
+            Assert.assertTrue(false);
+        }else{
+            Assert.assertEquals(title, driver.getTitle());
+        }
+    }
+
+    @When("User clicks on Logout link")
+    public void user_clicks_on_logout_link() {
+        lp.clickLogout();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Then("Close the browser")
+    public void close_the_browser() {
+        driver.quit();
+    }
+
+
+}
